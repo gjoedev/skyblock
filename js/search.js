764145e5fa84d98a2ffdc50aa5bb67b3eaccfd31
gjoedev <74843242+gjoedev@users.noreply.github.com>
2022-07-25 19:22:06 -0400

    did some stuff

---
+++ js/search.js
@@ -0,0 +1,8 @@
+const f = document.getElementById("form")
+const q = document.getElementById("query")
+
+function submit(event){
+    event.preventDefault();
+}
+
+f.addEventListener(`submit`, submit)
